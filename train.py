c99e93a3c52069d047c171474d2c3d5d38d3ccb8
Cristobal <cridonoso@inf.udec.cl>
2021-02-20 13:17:19 -0300

    voc size

--- train.py
+++ train.py
@@ -21,9 +21,9 @@ from core.callbacks import get_callbacks
 logging.getLogger('tensorflow').setLevel(logging.ERROR)  # suppress warnings
 
 # Hyperparameters
-num_layers = 4
-d_model = 128
-dff = 512
+num_layers = 6
+d_model = 512
+dff = 2048
 num_heads = 8
 dropout_rate = 0.1
 exp_path = "./experiments/train"
