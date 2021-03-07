0e7419aeaf6597f401ee44731f1652572fc93a09
Cristobal <cridonoso@inf.udec.cl>
2021-03-06 23:13:52 -0300

    setup and testing metrics saved

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
+import argparse
 import logging
+import json
 import time
 import os
 
@@ -12,44 +14,79 @@ from core.metrics import CustomACC
 
 logging.getLogger('tensorflow').setLevel(logging.ERROR)  # suppress warnings
 
-# Hyperparameters
-num_layers = 2 # stacked encoders 
-d_model = 256 # Head-attention Dimensionality 
-dff = 2048 # Middle Dense Layer Dimensionality
-num_heads = 4
-dropout_rate = 0.1
-exp_path = "./experiments/train"
-root_data = './data/records/macho'
-EPOCHS = 2
-BATCHSIZE = 16
+def train(opt):
+    # Loading data
+    train_batches = load_records(os.path.join(opt.data, 'train'), opt.batch_size)
+    valid_batches = load_records(os.path.join(opt.data, 'val'), opt.batch_size)
+    test_batches = load_records(os.path.join(opt.data, 'test'), opt.batch_size)
 
-# Loading data
-train_batches = load_records(os.path.join(root_data, 'train'), BATCHSIZE)
-valid_batches = load_records(os.path.join(root_data, 'val'), BATCHSIZE)
-test_batches = load_records(os.path.join(root_data, 'test'), BATCHSIZE)
+    train_batches = test_batches.take(1)
+    valid_batches = test_batches.take(1)
+    test_batches = test_batches.take(1)
+    # Optimizer
+    learning_rate = CustomSchedule(opt.head_dim)
+    optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, 
+                                        epsilon=1e-9)
+    # Model Instance
+    transformer = ASTROMER(num_layers=opt.layers,
+                        d_model=opt.head_dim,
+                        num_heads=opt.heads,
+                        dff=opt.dff,
+                        pe_input=opt.pe,
+                        rate=opt.dropout)
+    # Compile
+    transformer.compile(optimizer=optimizer, 
+                        loss=ASTROMERLoss(),
+                        metrics=[CustomMSE(), CustomACC()])
+    # Create graph
+    transformer.model(opt.batch_size).summary()
+    # Training
+    transformer.fit(train_batches, 
+                    epochs=opt.epochs, 
+                    verbose=1,
+                    validation_data=valid_batches,
+                    callbacks=get_callbacks(opt.p))
+    # Testing
+    metrics = transformer.evaluate(test_batches)
 
-# Optimizer
-learning_rate = CustomSchedule(d_model)
-optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, 
-                                     epsilon=1e-9)
-# Model Instance
-transformer = ASTROMER(num_layers=num_layers,
-                       d_model=d_model,
-                       num_heads=num_heads,
-                       dff=dff,
-                       pe_input=1000,
-                       rate=0.1)
-# Compile
-transformer.compile(optimizer=optimizer, 
-                    loss=ASTROMERLoss(),
-                    metrics=[CustomMSE(), CustomACC()])
-# Create graph
-transformer.model(BATCHSIZE).summary()
-# Training
-transformer.fit(train_batches, 
-                epochs=EPOCHS, 
-                verbose=1,
-                validation_data=valid_batches,
-                callbacks=get_callbacks())
-# Testing
-transformer.evaluate(test_batches)
+    # Saving metrics and setup file
+    os.makedirs(os.path.join(opt.p, 'test'), exist_ok=True)
+    test_file = os.path.join(opt.p, 'test/test_metrics.json')
+    with open(test_file, 'w') as json_file:
+        json.dump({'loss': metrics[0], 
+                   'mse':metrics[1], 
+                   'accuracy':metrics[2]}, json_file, indent=4)
+
+    conf_file = os.path.join(opt.p, 'conf.json')
+    with open(conf_file, 'w') as json_file:
+        json.dump(vars(opt), json_file, indent=4)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # TRAINING PAREMETERS
+    parser.add_argument('--data', default='./data/records/macho', type=str,
+                        help='Dataset folder containing the records files')
+    parser.add_argument('--p', default="./experiments/macho", type=str,
+                        help='Proyect path. Here will be stored weights and metrics')
+    parser.add_argument('--batch-size', default=128, type=int,
+                        help='batch size')
+    parser.add_argument('--epochs', default=1000, type=int,
+                        help='Number of epochs')
+    # ASTROMER HIPERPARAMETERS
+    parser.add_argument('--layers', default=2, type=int,
+                        help='Number of encoder layers')
+    parser.add_argument('--heads', default=4, type=int,
+                        help='Number of self-attention heads')
+    parser.add_argument('--head-dim', default=512, type=int,
+                        help='Head-attention Dimensionality ')
+    parser.add_argument('--dff', default=2048, type=int,
+                        help='Dimensionality of the middle  dense layer at the end of the encoder')
+    parser.add_argument('--pe', default=1000, type=int,
+                        help='Positional encoding maximum length')
+    parser.add_argument('--dropout', default=0.1, type=float,
+                        help='dropout_rate for the encoder')
+
+    opt = parser.parse_args()
+
+    train(opt)
