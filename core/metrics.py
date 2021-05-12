9489269c59204383dc92064cac68b247f1850d5f
Cristobal <cridonoso@inf.udec.cl>
2021-05-12 09:15:48 -0400

    updated loss

--- core/metrics.py
+++ core/metrics.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 
+@tf.function
 def custom_acc(y_true, y_pred):
     y_true = tf.reshape(y_true, [-1, 1])
     y_true = tf.cast(y_true, tf.int64)
+
     y_pred = tf.argmax(y_pred, 2)
+
 
     res = tf.math.equal(y_true, y_pred)
 
