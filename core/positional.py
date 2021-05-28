64f20142bf653b8553a33c71d1843e21d90d6fc7
Cristobal <cridonoso@inf.udec.cl>
2021-05-28 00:39:40 -0400

    pe proposed

--- core/positional.py
+++ core/positional.py
@@ -2,31 +2,31 @@ import tensorflow as tf
 import numpy as np
 
 
-# def get_angles(times, d_model, base=10000):
-#     with tf.name_scope("Get_Angles") as scope:
-#         t_init = tf.math.log(3/24) # 3 hours in days
-#         t_end = tf.math.log(3.*365) #3 years in days
-#         # Sample in log space
-#         exponents = tf.linspace(t_init, t_end, d_model)
-#         # Transform to normal space
-#         angle_rates = tf.math.divide_no_nan(1.0,tf.exp(exponents))
-#         # Compute the angular frequency
-#         angle_rates = 2*3.14159*angle_rates
-#         # Compute the argument of the trig functions
-#         angle_rates = times * angle_rates
-#         return angle_rates
-
 def get_angles(times, d_model, base=10000):
     with tf.name_scope("Get_Angles") as scope:
-        dim_indices = tf.range(d_model, dtype=tf.float32)
-
-        exponent = tf.divide(tf.multiply(2., tf.divide(dim_indices, 2.)),
-                             tf.cast(d_model, tf.float32))
-
-        angle_rates = tf.pow(tf.cast(base, dtype=tf.float32), exponent)
-        angle_rates = tf.math.reciprocal(angle_rates)
+        t_init = tf.math.log(3/24) # 3 hours in days
+        t_end = tf.math.log(3.*365) #3 years in days
+        # Sample in log space
+        exponents = tf.linspace(t_init, t_end, d_model)
+        # Transform to normal space
+        angle_rates = tf.math.divide_no_nan(1.0,tf.exp(exponents))
+        # Compute the angular frequency
+        angle_rates = 2*3.14159*angle_rates
+        # Compute the argument of the trig functions
         angle_rates = times * angle_rates
         return angle_rates
+
+# def get_angles(times, d_model, base=10000):
+#     with tf.name_scope("Get_Angles") as scope:
+#         dim_indices = tf.range(d_model, dtype=tf.float32)
+#
+#         exponent = tf.divide(tf.multiply(2., tf.divide(dim_indices, 2.)),
+#                              tf.cast(d_model, tf.float32))
+#
+#         angle_rates = tf.pow(tf.cast(base, dtype=tf.float32), exponent)
+#         angle_rates = tf.math.reciprocal(angle_rates)
+#         angle_rates = times * angle_rates
+#         return angle_rates
 
 def positional_encoding(times, d_model, base=10000, mjd=False):
     with tf.name_scope("PosEncoding") as scope:
