744b28869dd7d8d67ade13b17bab3445e0a38fe0
cristobal <cridonoso@udec.cl>
2021-09-24 09:07:46 -0300

    Sauce layer

--- core/classifier.py
+++ core/classifier.py
@@ -11,6 +11,7 @@ from core.astromer import get_ASTROMER
 from core.metrics import custom_acc
 from core.tboard import save_scalar
 from core.losses import custom_bce
+from core.output import SauceLayer
 from tqdm import tqdm
 
 from core.data import standardize
@@ -37,9 +38,9 @@ def get_fc_attention(units, num_classes, weigths):
     encoder.trainable = False
 
     x = encoder(encoder.input)
-    print(x)
-    x = tf.reduce_mean(x, 2)
-    x = tf.reshape(x, [-1, conf['max_obs']])
+    x = SauceLayer(200)(x)
+    # x = tf.reduce_mean(x, 2)
+    # x = tf.reshape(x, [-1, conf['max_obs']])
     print(x)
     x = LayerNormalization()(x)
     x = Dense(1024, name='FCN1')(x)
