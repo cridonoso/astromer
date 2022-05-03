f6f22c92f2dd3d492f54767f008014e5b416eea1
Cristobal <cridonoso@inf.udec.cl>
2022-05-03 20:35:38 +0000

    requirements.txt

--- presentation/experiments/clf/finetuning.py
+++ presentation/experiments/clf/finetuning.py
@@ -9,30 +9,49 @@ gpu = sys.argv[1]
 ds_name = sys.argv[2]
 astroweights = sys.argv[3]
 
-project_path = './presentation/experiments/clf/{}'.format(ds_name)
+project_path = './presentation/experiments/clf/finetuning/{}'.format(ds_name)
 
 datasets = ['{}_20'.format(ds_name),
             '{}_50'.format(ds_name),
             '{}_100'.format(ds_name),
             '{}_500'.format(ds_name)]
 
-for dataset in datasets:
-    print(dataset)
-    for fold_n in range(3):
-        start = time.time()
-        command1 = 'python -m presentation.scripts.train \
-                   --data ./data/records/{}/fold_{}/{} \
-                   --w {} \
-                   --batch-size 2048 \
-                   --p {}/fold_{}/{} \
-                   --gpu {}'.format(ds_name, fold_n, dataset,
-                                    astroweights,
-                                    project_path, fold_n, dataset,
-                                    gpu)
-        try:
-            subprocess.call(command1, shell=True)
-        except Exception as e:
-            print(e)
+start = time.time()
+command1 = 'python -m presentation.scripts.train \
+           --data ./data/records/{}/fold_{}/{} \
+           --w {} \
+           --batch-size 2048 \
+           --p {}/fold_{}/{} \
+           --gpu {}'.format(ds_name, gpu, ds_name,
+                            astroweights,
+                            project_path, gpu, ds_name,
+                            gpu)
+try:
+    subprocess.call(command1, shell=True)
+except Exception as e:
+    print(e)
 
-        end = time. time()
-        print('{} takes {:.2f} sec'.format(dataset, (end - start)))
+end = time. time()
+print('{} takes {:.2f} sec'.format(ds_name, (end - start)))
+
+        
+# for dataset in [ds_name]:
+#     print(dataset)
+#     for fold_n in range(3):
+#         start = time.time()
+#         command1 = 'python -m presentation.scripts.train \
+#                    --data ./data/records/{}/fold_{}/{} \
+#                    --w {} \
+#                    --batch-size 2048 \
+#                    --p {}/fold_{}/{} \
+#                    --gpu {}'.format(ds_name, fold_n, dataset,
+#                                     astroweights,
+#                                     project_path, fold_n, dataset,
+#                                     gpu)
+#         try:
+#             subprocess.call(command1, shell=True)
+#         except Exception as e:
+#             print(e)
+
+#         end = time. time()
+#         print('{} takes {:.2f} sec'.format(dataset, (end - start)))
