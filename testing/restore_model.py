f860957e1b1f6609fd6dfd1710fd9ab604d00b44
Cristobal <cridonoso@udec.cl>
2022-06-23 13:29:54 -0400

    restore model

--- testing/restore_model.py
+++ testing/restore_model.py
@@ -10,7 +10,7 @@ from core.training.metrics import custom_r2
 class TestStringMethods(unittest.TestCase):
 
     def test_model_save(self):
-        data = './data/records/alcock/fold_0/alcock_20/train'
+        data = './data/records/macho/test'
         dataset = load_dataset(data)
         dataset = pretraining_pipeline(dataset,
                                        batch_size=256,
@@ -21,47 +21,84 @@ class TestStringMethods(unittest.TestCase):
 
         model = ASTROMER()
 
-        w_path = './weights/macho_10022021/weights'
+        w_path = './weights/macho_10022021/'
+        model.load_weights(w_path)
 
-        reader = tf.train.load_checkpoint(w_path)
+        model.compile(optimizer='adam',
+                      loss_rec=custom_rmse,
+                      metric_rec=custom_r2)
+        model.evaluate(dataset.take(10))
 
-        shape_from_key = reader.get_variable_to_shape_map()
-
-
-        bias_0 = reader.get_tensor('layer_with_weights-0/enc_layers/0/ffn/layer_with_weights-0/bias/.ATTRIBUTES/VARIABLE_VALUE')
-        kernel_0 = reader.get_tensor('layer_with_weights-0/enc_layers/0/ffn/layer_with_weights-0/kernel/.ATTRIBUTES/VARIABLE_VALUE')
-        bias_1 = reader.get_tensor('layer_with_weights-0/enc_layers/0/ffn/layer_with_weights-1/bias/.ATTRIBUTES/VARIABLE_VALUE')
-        kernel_1 = reader.get_tensor('layer_with_weights-0/enc_layers/0/ffn/layer_with_weights-1/kernel/.ATTRIBUTES/VARIABLE_VALUE')
-
-        print(bias_0.shape)
-        print(kernel_0.shape)
-        print(bias_1.shape)
-        print(kernel_1.shape)
-        dense_layer_0 = model.get_layer('encoder')
-
-        print('----'*10)
-        for k in dense_layer_0.inp_transform.variables:
-            print(k.shape)
+        # reader = tf.train.load_checkpoint(w_path)
         #
-        # dense_layer_0.inp_transform.set_weights([kernel_0, bias_0])
+        # shape_from_key = reader.get_variable_to_shape_map()
         #
-        # print(dense_layer_0.inp_transform.weights)
+        # # ====== FIRST LAYER =====
+        # for l_index in range(2):
+        #     bias_0   = reader.get_tensor('layer_with_weights-0/enc_layers/{}/ffn/layer_with_weights-0/bias/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     kernel_0 = reader.get_tensor('layer_with_weights-0/enc_layers/{}/ffn/layer_with_weights-0/kernel/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     bias_1   = reader.get_tensor('layer_with_weights-0/enc_layers/{}/ffn/layer_with_weights-1/bias/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     kernel_1 = reader.get_tensor('layer_with_weights-0/enc_layers/{}/ffn/layer_with_weights-1/kernel/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #
+        #     beta_0  = reader.get_tensor('layer_with_weights-0/enc_layers/{}/layernorm1/beta/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     gamma_0 = reader.get_tensor('layer_with_weights-0/enc_layers/{}/layernorm1/gamma/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     beta_1  = reader.get_tensor('layer_with_weights-0/enc_layers/{}/layernorm2/beta/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     gamma_1 = reader.get_tensor('layer_with_weights-0/enc_layers/{}/layernorm2/gamma/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #
+        #     mha_bias   = reader.get_tensor('layer_with_weights-0/enc_layers/{}/mha/dense/bias/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     mha_kernel = reader.get_tensor('layer_with_weights-0/enc_layers/{}/mha/dense/kernel/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #
+        #
+        #     first_fnn = model.get_layer('encoder').enc_layers[l_index].ffn
+        #     first_fnn.set_weights([
+        #         kernel_0, bias_0, kernel_1, bias_1
+        #     ])
+        #
+        #     layernorm1 = model.get_layer('encoder').enc_layers[l_index].layernorm1
+        #     layernorm2 = model.get_layer('encoder').enc_layers[l_index].layernorm2
+        #     layernorm1.set_weights([gamma_0, beta_0])
+        #     layernorm2.set_weights([gamma_1, beta_1])
+        #
+        #     mixer_dense_0 = model.get_layer('encoder').enc_layers[l_index].mha.dense
+        #     mixer_dense_0.set_weights([mha_kernel, mha_bias])
+        #
+        #     wk_0_bias   = reader.get_tensor('layer_with_weights-0/enc_layers/{}/mha/wk/bias/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     wk_0_kernel = reader.get_tensor('layer_with_weights-0/enc_layers/{}/mha/wk/kernel/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     wq_0_bias   = reader.get_tensor('layer_with_weights-0/enc_layers/{}/mha/wq/bias/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     wq_0_kernel = reader.get_tensor('layer_with_weights-0/enc_layers/{}/mha/wq/kernel/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     wv_0_bias   = reader.get_tensor('layer_with_weights-0/enc_layers/{}/mha/wv/bias/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #     wv_0_kernel = reader.get_tensor('layer_with_weights-0/enc_layers/{}/mha/wv/kernel/.ATTRIBUTES/VARIABLE_VALUE'.format(l_index))
+        #
+        #     wk_0 = model.get_layer('encoder').enc_layers[l_index].mha.wk
+        #     wk_0.set_weights([wk_0_kernel, wk_0_bias])
+        #
+        #     wq_0 = model.get_layer('encoder').enc_layers[l_index].mha.wq
+        #     wq_0.set_weights([wq_0_kernel, wq_0_bias])
+        #
+        #     wv_0 = model.get_layer('encoder').enc_layers[l_index].mha.wv
+        #     wv_0.set_weights([wv_0_kernel, wv_0_bias])
+        #
+        #
+        #
+        # inp_t_bias   = reader.get_tensor('layer_with_weights-0/inp_transform/bias/.ATTRIBUTES/VARIABLE_VALUE')
+        # inp_t_kernel = reader.get_tensor('layer_with_weights-0/inp_transform/kernel/.ATTRIBUTES/VARIABLE_VALUE')
+        #
+        # bn_0_beta  = reader.get_tensor('layer_with_weights-1/bn_0/beta/.ATTRIBUTES/VARIABLE_VALUE')
+        # bn_0_gamma = reader.get_tensor('layer_with_weights-1/bn_0/gamma/.ATTRIBUTES/VARIABLE_VALUE')
+        #
+        # reg_layer_bias   = reader.get_tensor('layer_with_weights-1/reg_layer/bias/.ATTRIBUTES/VARIABLE_VALUE')
+        # reg_layer_kernel = reader.get_tensor('layer_with_weights-1/reg_layer/kernel/.ATTRIBUTES/VARIABLE_VALUE')
+        #
+        # inp_transform = model.get_layer('encoder').inp_transform
+        # inp_transform.set_weights([inp_t_kernel, inp_t_bias])
+        #
+        # reg_layer = model.get_layer('regression').reg_layer
+        # bn_0 = model.get_layer('regression').bn_0
+        # reg_layer.set_weights([reg_layer_kernel, reg_layer_bias])
+        # bn_0.set_weights([bn_0_gamma, bn_0_beta])
+        #
+        # model.save_weights('./weights/macho_10022021/weights_old.h5')
 
-        print('----'*10)
-        for v in sorted(shape_from_key.keys()):
-            print(v)
-
-        # #
-        # for v in model.variables:
-        #     print(v.name)
-
-        # a = reader.get_tensor('layer_with_weights-0/enc_layers/0/ffn/layer_with_weights-0/bias/.ATTRIBUTES/VARIABLE_VALUE')
-
-        # model.load_weights()
-        # model.compile(optimizer='adam',
-        #               loss_rec=custom_rmse,
-        #               metric_rec=custom_r2)
-        # model.evaluate(dataset)
 
 
 
