7adbe6f84e008633d902ff8d2c260c5307529c44
Cristobal <cridonoso@udec.cl>
2022-06-23 23:16:02 -0400

    a and b tgther

--- presentation/scripts/clf_script.py
+++ presentation/scripts/clf_script.py
@@ -13,14 +13,12 @@ batch_size = 512
 
 if science_case == 'a':
     train_astromer = False
-    sc_ft = 'ab'
-
-if science_case == 'b':
-    train_astromer = True
+    science_cases = ['a', 'b']
     sc_ft = 'ab'
 
 if science_case == 'c':
     train_astromer = True
+    science_cases = ['c']
     sc_ft = 'c'
 
 datasets = ['{}_20'.format(ds_name),
@@ -29,38 +27,39 @@ datasets = ['{}_20'.format(ds_name),
             '{}_500'.format(ds_name),
             ]
 
-for dataset in datasets:
-    for fold_n in range(3):
-        for mode in ['lstm_att', 'mlp_att', 'lstm']:
-            print('{} on mode {}'.format(dataset, mode))
-            astroweights = './runs/astromer_{}/{}/{}/fold_{}/{}'.format(astromer_dim,
-                                                                        sc_ft,
-                                                                        ds_name,
-                                                                        fold_n,
-                                                                        dataset)
+for science_case in science_cases:
+    for dataset in datasets:
+        for fold_n in range(3):
+            for mode in ['lstm_att', 'mlp_att', 'lstm']:
+                print('sc:{} - {} on mode {}'.format(science_case, dataset, mode))
+                astroweights = './runs/astromer_{}/{}/{}/fold_{}/{}'.format(astromer_dim,
+                                                                            sc_ft,
+                                                                            ds_name,
+                                                                            fold_n,
+                                                                            dataset)
 
-            project_path = './runs/astromer_{}/classifiers/{}/{}/fold_{}/{}'.format(astromer_dim,
-                                                                                    science_case,
-                                                                                    ds_name,
-                                                                                    fold_n,
-                                                                                    dataset)
+                project_path = './runs/astromer_{}/classifiers/{}/{}/fold_{}/{}'.format(astromer_dim,
+                                                                                        science_case,
+                                                                                        ds_name,
+                                                                                        fold_n,
+                                                                                        dataset)
 
-            command1 = 'python -m presentation.scripts.classify \
-                            --data ./data/records/{}/fold_{}/{} \
-                            --p {} \
-                            --w {} \
-                            --batch-size {} \
-                            --mode {} \
-                            --gpu {}'.format(ds_name, fold_n, dataset,
-                                             project_path,
-                                             astroweights,
-                                             batch_size,
-                                             mode,
-                                             gpu)
-            if train_astromer:
-                command1 += ' --finetune'
+                command1 = 'python -m presentation.scripts.classify \
+                                --data ./data/records/{}/fold_{}/{} \
+                                --p {} \
+                                --w {} \
+                                --batch-size {} \
+                                --mode {} \
+                                --gpu {}'.format(ds_name, fold_n, dataset,
+                                                 project_path,
+                                                 astroweights,
+                                                 batch_size,
+                                                 mode,
+                                                 gpu)
+                if train_astromer:
+                    command1 += ' --finetune'
 
-            try:
-                subprocess.call(command1, shell=True)
-            except Exception as e:
-                print(e)
+                try:
+                    subprocess.call(command1, shell=True)
+                except Exception as e:
+                    print(e)
