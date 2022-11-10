5e42a1f8e1cabcc80df849dfc5077d7c19a81e4e
cristobal <cridonoso@inf.udec.cl>
2022-11-10 20:04:18 -0300

    pretraining script

--- presentation/scripts/pretraining.py
+++ presentation/scripts/pretraining.py
@@ -6,11 +6,15 @@ import pandas as pd
 import os, sys
 
 from src.data import pretraining_pipeline
-from tensorflow.keras.callbacks  import EarlyStopping, TensorBoard
+from src.models import get_ASTROMER
+
+from tensorflow.keras.callbacks  import (ModelCheckpoint,
+                                         EarlyStopping,
+                                         TensorBoard)
 from tensorflow.keras.optimizers import Adam
 
 
-os.environ["CUDA_VISIBLE_DEVICES"] = sys.argv[2]
+os.environ["CUDA_VISIBLE_DEVICES"] = '-1' if len(sys.argv) == 1 else sys.argv[1]
 
 exp_path = './presentation/scripts/results'
 head_dim = 64
@@ -28,13 +32,53 @@ astromer =  get_ASTROMER(num_layers=2,
                          no_train=False)
 optimizer = Adam(1e-3, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
 astromer.compile(optimizer=optimizer)
-pretrained_weigths = './weights/macho'
+pretrained_weigths = './weights/macho/weights'
 astromer.load_weights(pretrained_weigths)
 
-#
+# LOADING DATA
+BATCH_SIZE = 256
+train_batches = pretraining_pipeline('./data/records/macho/train',
+                                     batch_size=BATCH_SIZE,
+                                     window_size=200,
+                                     msk_frac=.5,
+                                     rnd_frac=.2,
+                                     same_frac=.2,
+                                     sampling=True,
+                                     shuffle=True,
+                                     repeat=4,
+                                     normalize=True,
+                                     cache=True)
+valid_batches = pretraining_pipeline('./data/records/macho/val',
+                                     batch_size=BATCH_SIZE,
+                                     window_size=200,
+                                     msk_frac=.5,
+                                     rnd_frac=.2,
+                                     same_frac=.2,
+                                     sampling=True,
+                                     shuffle=False,
+                                     repeat=1,
+                                     normalize=True,
+                                     cache=True)
+train_batches = train_batches.take(5) # for testing purposes only
+valid_batches = valid_batches.take(5) # for testing purposes only
 
-# # Train ASTROMER
-# _ = astromer.fit(data['train'],
-#               epochs=config[step]['epochs'],
-#               validation_data=data['val'],
-#               callbacks=cbks)
+# CALLBACKS
+callbacks = [
+    ModelCheckpoint(
+        filepath=os.path.join(exp_path, 'weights'),
+        save_weights_only=True,
+        monitor='val_loss',
+        save_best_only=True),
+    EarlyStopping(monitor='val_loss',
+        patience = 40,
+        restore_best_weights=True),
+    TensorBoard(
+        log_dir = os.path.join(exp_path, 'logs'),
+        histogram_freq=1,
+        write_graph=False)]
+
+# TRAIN ASTROMER
+_ = astromer.fit(train_batches,
+                 epochs=10,
+                 validation_data=valid_batches,
+                 callbacks=callbacks)
