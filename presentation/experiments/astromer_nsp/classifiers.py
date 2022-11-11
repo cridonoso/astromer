0d5ade94e8825566d1ad58ca3641df0e6ba037ae
cristobal <cridonoso@inf.udec.cl>
2022-11-11 17:48:17 -0300

    nsp

--- presentation/experiments/astromer_nsp/classifiers.py
+++ presentation/experiments/astromer_nsp/classifiers.py
@@ -38,17 +38,18 @@ class NormedLSTMCell(tf.keras.layers.Layer):
 
 def build_lstm(maxlen, n_classes):
     print('[INFO] Building LSTM Baseline')
-    serie  = Input(shape=(maxlen, 1), batch_size=None, name='input')
-    times  = Input(shape=(maxlen, 1), batch_size=None, name='times')
-    mask   = Input(shape=(maxlen, 1), batch_size=None, name='mask')
+    serie  = Input(shape=(maxlen+3, 1), batch_size=None, name='input')
+    times  = Input(shape=(maxlen+3, 1), batch_size=None, name='times')
+    mask   = Input(shape=(maxlen+3, 1), batch_size=None, name='mask')
 
     placeholder = {'input':serie,
                    'mask_in':mask,
                    'times':times}
 
-    m = tf.cast(1.-placeholder['mask_in'][...,0], tf.bool)
+    m = tf.cast(placeholder['mask_in'][...,0], tf.bool)
     tim = normalize_batch(placeholder['times'])
     inp = normalize_batch(placeholder['input'])
+
     x = tf.concat([tim, inp], 2)
 
     cell_0 = NormedLSTMCell(units=256)
@@ -66,13 +67,17 @@ def build_lstm(maxlen, n_classes):
     return Model(placeholder, outputs=x, name="LSTM")
 
 def build_lstm_att(astromer, maxlen, n_classes, train_astromer=False):
-    serie  = Input(shape=(maxlen, 1), batch_size=None, name='input')
-    times  = Input(shape=(maxlen, 1), batch_size=None, name='times')
-    mask   = Input(shape=(maxlen, 1), batch_size=None, name='mask')
-    print('BUILDING LSTM + ATT')
+    serie   = Input(shape=(maxlen+3, 1), batch_size=None, name='input')
+    times   = Input(shape=(maxlen+3, 1), batch_size=None, name='times')
+    mask_in = Input(shape=(maxlen+3, 1), batch_size=None, name='mask_in')
+    mask    = Input(shape=(maxlen+3, 1), batch_size=None, name='mask')
+    print('BUILDING MLP + ATT')
+
     placeholder = {'input':serie,
-                   'mask_in':mask,
+                   'mask':mask,
+                   'mask_in':mask_in,
                    'times':times}
+
 
     cell_0 = NormedLSTMCell(units=256)
     dense  = Dense(n_classes, name='FCN')
@@ -86,7 +91,9 @@ def build_lstm_att(astromer, maxlen, n_classes, train_astromer=False):
     encoder = astromer.get_layer('encoder')
     encoder.trainable = train_astromer
 
-    mask = tf.cast(1.-placeholder['mask_in'][...,0], dtype=tf.bool)
+    mask = placeholder['mask']
+    mask = tf.cast(1.-placeholder['mask'][...,0], dtype=tf.bool)
+
     x = encoder(placeholder, training=train_astromer)
     x = tf.math.divide_no_nan(x-tf.expand_dims(tf.reduce_mean(x, 1),1),
                               tf.expand_dims(tf.math.reduce_std(x, 1), 1))
@@ -96,19 +103,21 @@ def build_lstm_att(astromer, maxlen, n_classes, train_astromer=False):
     return Model(placeholder, outputs=x, name="LSTM_ATT")
 
 def build_mlp_att(astromer, maxlen, n_classes, train_astromer=False):
-    serie  = Input(shape=(maxlen, 1), batch_size=None, name='input')
-    times  = Input(shape=(maxlen, 1), batch_size=None, name='times')
-    mask   = Input(shape=(maxlen, 1), batch_size=None, name='mask')
+    serie   = Input(shape=(maxlen+3, 1), batch_size=None, name='input')
+    times   = Input(shape=(maxlen+3, 1), batch_size=None, name='times')
+    mask_in = Input(shape=(maxlen+3, 1), batch_size=None, name='mask_in')
+    mask    = Input(shape=(maxlen+3, 1), batch_size=None, name='mask')
     print('BUILDING MLP + ATT')
 
     placeholder = {'input':serie,
-                   'mask_in':mask,
+                   'mask':mask,
+                   'mask_in':mask_in,
                    'times':times}
 
     encoder = astromer.get_layer('encoder')
     encoder.trainable = train_astromer
 
-    mask = 1.-placeholder['mask_in']
+    mask = placeholder['mask']
 
     x = encoder(placeholder, training=train_astromer)
     x = x * mask
@@ -119,32 +128,65 @@ def build_mlp_att(astromer, maxlen, n_classes, train_astromer=False):
     x = Dense(256, activation='relu')(x)
     x = LayerNormalization()(x)
     x = Dense(n_classes)(x)
+
     return Model(inputs=placeholder, outputs=x, name="FCATT")
 
-def build_mlp_att_cls(astromer, maxlen, n_classes, train_astromer=False):
-    serie  = Input(shape=(maxlen, 1), batch_size=None, name='input')
-    times  = Input(shape=(maxlen, 1), batch_size=None, name='times')
-    mask   = Input(shape=(maxlen, 1), batch_size=None, name='mask')
-    print('BUILDING MLP + ATT')
+def build_mlp_cls(astromer, maxlen, n_classes, train_astromer=False):
+    serie   = Input(shape=(maxlen+3, 1), batch_size=None, name='input')
+    times   = Input(shape=(maxlen+3, 1), batch_size=None, name='times')
+    mask_in = Input(shape=(maxlen+3, 1), batch_size=None, name='mask_in')
+    mask    = Input(shape=(maxlen+3, 1), batch_size=None, name='mask')
+    print('BUILDING MLP + CLS')
 
     placeholder = {'input':serie,
-                   'mask_in':mask,
+                   'mask':mask,
+                   'mask_in':mask_in,
                    'times':times}
 
     encoder = astromer.get_layer('encoder')
     encoder.trainable = train_astromer
-        
-    mask = 1.-placeholder['mask_in']
 
     x = encoder(placeholder, training=train_astromer)
     x = tf.slice(x, [0,0,0], [-1,1,-1])
-    
+    x = tf.reshape(x, [tf.shape(x)[0], tf.shape(x)[-1]])
     x = Dense(1024, activation='relu')(x)
     x = Dense(512, activation='relu')(x)
     x = Dense(256, activation='relu')(x)
     x = LayerNormalization()(x)
     x = Dense(n_classes)(x)
-    return Model(inputs=placeholder, outputs=x, name="FCATT")
+    return Model(inputs=placeholder, outputs=x, name="FCCLS")
+
+def build_mlp_att_cls(astromer, maxlen, n_classes, train_astromer=False):
+    serie   = Input(shape=(maxlen+3, 1), batch_size=None, name='input')
+    times   = Input(shape=(maxlen+3, 1), batch_size=None, name='times')
+    mask_in = Input(shape=(maxlen+3, 1), batch_size=None, name='mask_in')
+    mask    = Input(shape=(maxlen+3, 1), batch_size=None, name='mask')
+    print('BUILDING MLP + ATT + CLS')
+
+    placeholder = {'input':serie,
+                   'mask':mask,
+                   'mask_in':mask_in,
+                   'times':times}
+
+    encoder = astromer.get_layer('encoder')
+    encoder.trainable = train_astromer
+
+    mask = placeholder['mask']
+
+    x = encoder(placeholder, training=train_astromer)
+
+    x_rest = x * mask
+    x_rest = tf.reduce_sum(x_rest, 1)/tf.reduce_sum(mask, 1)
+    x_cls = tf.slice(x, [0,0,0], [-1,1,-1])
+    x_cls = tf.reshape(x_cls, [tf.shape(x_cls)[0], tf.shape(x_cls)[-1]])
+
+    x = tf.concat([x_rest, x_cls], 1)
+    x = Dense(1024, activation='relu')(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dense(256, activation='relu')(x)
+    x = LayerNormalization()(x)
+    x = Dense(n_classes)(x)
+    return Model(inputs=placeholder, outputs=x, name="FCATTCLS")
 
 def get_classifier_by_name(name, config, astromer=None, train_astromer=False):
     num_cls = pd.read_csv(
@@ -166,6 +208,11 @@ def get_classifier_by_name(name, config, astromer=None, train_astromer=False):
                                  config['astromer']['window_size'],
                                  num_cls,
                                  train_astromer=train_astromer)
+    if name == 'mlp_cls':
+        clf_model= build_mlp_cls(astromer,
+                                     config['astromer']['window_size'],
+                                     num_cls,
+                                     train_astromer=train_astromer)
     if name == 'mlp_att_cls':
         clf_model= build_mlp_att_cls(astromer,
                                      config['astromer']['window_size'],
