5919808d9e8353ef7a2f4b137327a2f5658fb59c
Cristobal Donoso-Oliva <cridonoso@inf.udec.cl>
2023-03-17 18:19:05 -0300

    scheudler

--- src/training/scheduler.py
+++ src/training/scheduler.py
@@ -7,7 +7,6 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
 
         self.d_model = d_model
         self.d_model = tf.cast(self.d_model, tf.float32)
-        self.sf = tf.pow(10.,scale)
         self.warmup_steps = warmup_steps
 
     def __call__(self, step):
@@ -15,4 +14,12 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         arg1 = tf.math.rsqrt(step)
         arg2 = step * (self.warmup_steps ** -1.5)
 
-        return (tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2))*self.sf
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "d_model": self.d_model,
+            "warmup_steps": self.warmup_steps,
+        })
+        return config
