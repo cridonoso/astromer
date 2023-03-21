6f20844d6d999de824f2fa12d3ab7f93cee8af05
Cristobal Donoso-Oliva <cridonoso@inf.udec.cl>
2023-03-21 12:43:39 -0300

    masking before attention

--- src/layers/attention.py
+++ src/layers/attention.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def scaled_dot_product_attention(q, k, v, mask):
+def scaled_dot_product_attention(q, k, v, mask=None):
     """Calculate the attention weights.
     q, k, v must have matching leading dimensions.
     k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
@@ -25,12 +25,13 @@ def scaled_dot_product_attention(q, k, v, mask):
     dk = tf.cast(tf.shape(k)[-1], tf.float32)
     scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
 
-    steps = tf.shape(scaled_attention_logits)[2]
-    mask_rshp = tf.tile(mask, [1,1,steps])
-    mask_rshp += tf.transpose(mask_rshp, [0,2,1])
-    mask_rshp = tf.minimum(1., mask_rshp)
-    mask_rshp = tf.expand_dims(mask_rshp, 1)
-    scaled_attention_logits += mask_rshp
+    if mask is not None:
+        steps = tf.shape(scaled_attention_logits)[2]
+        mask_rshp = tf.tile(mask, [1,1,steps])
+        mask_rshp += tf.transpose(mask_rshp, [0,2,1])
+        mask_rshp = tf.minimum(1., mask_rshp)
+        mask_rshp = tf.expand_dims(mask_rshp, 1)
+        scaled_attention_logits += mask_rshp
 
     # softmax is normalized on the last axis (seq_len_k) so that the scores add up to 1.
     attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1, name='MaskedSoftMax')  # (..., seq_len_q, seq_len_k)
@@ -64,6 +65,8 @@ class HeadAttentionMulti(tf.keras.layers.Layer):
     def call(self, x, mask):
         batch_size = tf.shape(x)[0]
 
+        x = tf.multiply(x, mask)
+
         q = self.wq(x)  # (batch_size, seq_len, d_model)
         k = self.wk(x)  # (batch_size, seq_len, d_model)
         v = self.wv(x)  # (batch_size, seq_len, d_model)
@@ -74,7 +77,7 @@ class HeadAttentionMulti(tf.keras.layers.Layer):
 
         # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
         # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
-        scaled_attention, attention_weights = scaled_dot_product_attention(q, k, v, mask)
+        scaled_attention, attention_weights = scaled_dot_product_attention(q, k, v)
 
         scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])  # (batch_size, seq_len_q, num_heads, depth)
 
