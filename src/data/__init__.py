1fd04fb22fdc8658b457a4043f3de250eeb48ee2
Cristobal Donoso-Oliva <cridonoso@inf.udec.cl>
2023-04-10 15:32:23 -0400

    Revert "Merge pull request #30 from cridonoso/new_pipeline_ibt"
    This reverts commit b736b01af771dc7ce0016edbbfbb53c6b16d2854, reversing
    changes made to b8887184979e7e8a7da9934cb0505d9758aaa76c.

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,3 +2,4 @@ from src.data.loaders import load_records, pretraining_pipeline
 from src.data.masking import mask_dataset, reshape_mask
 from src.data.preprocessing import to_windows, standardize
 from src.data.nsp import nsp_dataset
+from src.data.record import create_dataset
