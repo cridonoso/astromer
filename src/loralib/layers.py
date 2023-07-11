131bc4cdd139eb4dda1f1856ba69ba247da47e60
your_username <reachhvg@gmail.com>
2023-07-11 07:33:21 +0000

    fresh commit 11th july

--- src/loralib/layers.py
+++ src/loralib/layers.py
@@ -12,7 +12,7 @@ class LoRALayer(tf.Module):
     ):
         super(LoRALayer, self).__init__()
         self.r = r
-        self.lora_alpga = lora_alpha
+        self.lora_alpha = lora_alpha
         #dropout optional 
         if lora_dropout > 0.:
             self.lora_dropout = Dropout(lora_dropout)
@@ -23,57 +23,67 @@ class LoRALayer(tf.Module):
         self.merge_weights = merge_weights
 
 
-class Embedding(tf.keras.layers.Embedding, LoRALayer):
-    #LoRA implemented in dense layer 
-    def __init__(
-            self, 
-            num_embeddings: int, 
-            embedding_dim: int, 
-            r: int =0, 
-            lora_alpha: int =1, 
-            merge_weights: bool = True, 
-            **kwargs
-    ):
-        tf.keras.layers.Embedding.__init__(self, num_embeddings, embedding_dim, **kwargs)
-        LoRALayer.__init___(self, r=r, lora_alpha=lora_alpha, lora_dropout=0, merge_weights=merge_weights)
-        #Acutal trainable parameters 
-        if r > 0:
-            self.lora_A = self.add_weight(shape=(r, num_embeddings), trainable=True, initializer='zeros')
-            self.lora_B = self.add_weight(shape=(embedding_dim, r), trainable = True, initializer='zeros')
-            self.scaling = self.lora_alpha/ self.r
-            #freezing the pretrained weight matrix 
-            self.embeddings.trainable = False
-        self.reset_parameters()
+class Embedding(tf.keras.layers.Layer):
+    def __init__(self, num_embeddings, embedding_dim, r, lora_alpha, merge_weights=True):
+        super(Embedding, self).__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.merge_weights = merge_weights
+        self.lora_alpha = lora_alpha
+        self.embeddings = tf.Variable(
+            tf.random.normal([num_embeddings, embedding_dim]),
+            trainable=True,
+            name='embeddings')
+        self.r = r
+
+        # Low-Rank Approximation
+        self.lora_A = tf.Variable(
+            tf.random.normal([num_embeddings, r]),
+            trainable=True,
+            name='lora_A')
+        self.lora_B = tf.Variable(
+            tf.random.normal([r, embedding_dim]),
+            trainable=True,
+            name='lora_B')
+
+        self.merged = False
+        self.scaling = tf.math.sqrt(tf.cast(embedding_dim, dtype=tf.float32))
 
     def reset_parameters(self):
-        if hasattr(self, 'lora_A'):
-            #initialize A the sme way as the default for tf.keras.layers.Dense and B to zero
+        """Resets the LoRA parameters to their initial values."""
+        if self.r > 0:
             self.lora_A.assign(tf.zeros_like(self.lora_A))
-            self.lora_B.assign(tf.random.normal(self.lora_B.shape))
+            self.lora_B.assign(tf.zeros_like(self.lora_B))
+            self.merged = False
 
-    def train(self, mode: bool =True):
-        self.training = mode 
-        if mode:
-            if self.merge_weights and self.merged:
-                #make sure the weights are not merged 
-                if self.r > 0:
-                    self.embeddings.assign_sub(tf.linalg.matmul(self.lora_B, self.lora_A))
-                self.merged = False 
-        
-        else:
-            if self.merge_weights and not self.merged:
-                #merge the weights and mark it 
-                if self.r > 0:
-                    self.embeddings.assign_add(tf.linalg.matmul(self.lora_B, self.lora_A, transpose_a=True) * self.scaling)
-                self.merged = True
+    def build(self, input_shape):
+        pass
 
     def call(self, x):
         if self.r > 0 and not self.merged:
-            result = super().call(x)
-            if self.r > 0:
-                after_A = tf.nn.embedding_lookup(self.lora_A, x)
-                result += tf.linalg.matmul(after_A, self.lora_B, transpose_b = True) * self.scaling 
-                return result 
+            frequent_tokens = ...  # Define your top-r tokens based on your dataset
+            lora_embeddings = tf.nn.embedding_lookup(tf.transpose(self.lora_A), frequent_tokens)  # Shape: [batch_size, sequence_len, r]
+            lora_embeddings = tf.matmul(lora_embeddings, self.lora_B)  # We use matmul 
+            lora_embeddings = tf.reshape(lora_embeddings, tf.shape(result))  # Ensure lora_embeddings has the same shape as result
+            lora_embeddings *= self.scaling
         else:
-            return super().call(x)
+            lora_embeddings = 0
+        result = tf.nn.embedding_lookup(tf.transpose(self.embeddings), x) + lora_embeddings
+        return result
+
+    def train(self, training=True):
+        self.trainable = training
+        if not training and self.merge_weights and not self.merged:
+            self.embeddings.assign_add(tf.tensordot(self.lora_A, self.lora_B, axes=[[0], [0]]) * self.scaling)
+            self.merged = True
+        elif training:
+            self.merged = False
+            if hasattr(self, 'lora_A'):
+                self.lora_A.assign(tf.zeros_like(self.lora_A))
+            if hasattr(self, 'lora_B'):
+                self.lora_B.assign(tf.zeros_like(self.lora_B))
+
+
+
+
         
