405b3f1989d9a06979f78ae097487fead3f90464
Yash Surange <88226034+ysurs@users.noreply.github.com>
2023-10-02 10:26:02 +0530

    new attention block for astrospec

--- src/layers/attblock.py
+++ src/layers/attblock.py
@@ -14,7 +14,7 @@ class MergingLayer(tf.keras.layers.Layer):
 
 class AttentionBlock(tf.keras.layers.Layer):
 	def __init__(self, head_dim, num_heads, mixer_size, dropout=0.1, **kwargs):
-		super(AttentionBlock, self).__init__(**kwargs)
+		super(AttentionBlock, self).__init__(head_dim, num_heads, mixer_size, dropout=0.1, **kwargs)
 		self.head_dim = head_dim
 		self.num_heads = num_heads
 		self.mixer_size = mixer_size
@@ -33,6 +33,24 @@ class AttentionBlock(tf.keras.layers.Layer):
 		ffn_output  = self.ffn(attn_output)  # (batch_size, input_seq_len, d_model)
 		ffn_output  = self.dropout2(ffn_output, training=training)
 		ffn_output  = self.layernorm2(ffn_output, training=training)
+		if return_weights:
+			return ffn_output, att_weights
+		return ffn_output
+
+class AttentionBlock_astrospec(AttentionBlock):
+	def __init__(self, head_dim, num_heads, mixer_size, dropout=0.1, **kwargs):
+		super(AttentionBlock_astrospec, self).__init__(head_dim, num_heads, mixer_size, dropout=0.1, **kwargs)
+		self.skip_after_mha=tf.keras.layers.Dense(self.head_dim*self.num_heads)
+		self.skip_after_ffn=tf.keras.layers.Dense(self.head_dim*self.num_heads)
+
+	def call(self, x, training, mask=None, return_weights=False):
+		attn_output, att_weights = self.mha(x, mask)  # (batch_size, input_seq_len, d_model)
+		attn_output = self.dropout1(attn_output, training=training)
+		residual_mask=tf.where(tf.equal(mask,1), tf.zeros_like(mask), tf.ones_like(mask))
+		attn_output = self.layernorm1(self.skip_after_mha(x*residual_mask)+attn_output, training=training)
+		ffn_output  = self.ffn(attn_output)  # (batch_size, input_seq_len, d_model)
+		ffn_output  = self.dropout2(ffn_output, training=training)
+		ffn_output  = self.layernorm2(self.skip_after_ffn(attn_output)+ffn_output, training=training)
 		if return_weights:
 			return ffn_output, att_weights
 		return ffn_output
