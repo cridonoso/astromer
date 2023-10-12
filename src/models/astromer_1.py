39c15db6f9c0d6479ed03a7dd3b8aecbe9b9f847
cris <cridonoso@inf.udec.cl>
2023-10-11 22:09:12 -0300

    martina

--- src/models/astromer_1.py
+++ src/models/astromer_1.py
@@ -36,7 +36,8 @@ def get_ASTROMER(num_layers=2,
 				 window_size=100,
 				 batch_size=None,
 				 encoder_mode='normal',
-				 average_layers=False):
+				 average_layers=False,
+				 mask_format='first'):
 
 	placeholder = build_input(window_size)
 
@@ -50,6 +51,7 @@ def get_ASTROMER(num_layers=2,
 					  pe_dim=pe_dim,
 					  pe_c=pe_c,
 					  average_layers=average_layers,
+					  mask_format=mask_format,
 					  name='encoder')
 
 	x = encoder(placeholder)
