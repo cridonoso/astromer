364c7c3d34b1c23886b93a008d8286c69b657569
Cristobal Donoso <cridonoso@inf.udec.cl>
2024-09-02 18:52:21 -0400

    pipeline to download big macho

---
+++ presentation/pipelines/machofields/utils.py
@@ -0,0 +1,31 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+
+def connect_to_drive():
+    gauth = GoogleAuth()
+
+    # Try to load saved client credentials
+    gauth.LoadCredentialsFile("mycreds.txt")
+
+    if gauth.credentials is None:
+        # Authenticate if they're not there
+
+        # This is what solved the issues:
+        gauth.GetFlow()
+        gauth.flow.params.update({'access_type': 'offline'})
+        gauth.flow.params.update({'approval_prompt': 'force'})
+        gauth.LocalWebserverAuth()
+
+    elif gauth.access_token_expired:
+        # Refresh them if expired
+        gauth.Refresh()
+    else:
+        # Initialize the saved creds
+        gauth.Authorize()
+
+    # Save the current credentials to a file
+    gauth.SaveCredentialsFile("mycreds.txt")  
+    drive = GoogleDrive(gauth)
+
+    return drive
