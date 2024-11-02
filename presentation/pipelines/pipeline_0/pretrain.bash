819386260b89b2749ca449be7d99bfbf37bb96fa
Cristobal <cridonoso>
2024-11-02 01:26:07 -0300

    pretrain batch

--- presentation/pipelines/pipeline_0/pretrain.bash
+++ presentation/pipelines/pipeline_0/pretrain.bash
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# nsamples=(100 1000 10000 100000 250000 500000 750000)
+nsamples=(1000 10000 100000)
 
-# for str in ${nsamples[@]}; do
-#    echo TRAINING WITH $str 
-#    python -m presentation.scripts.pretrain --exp-name nsamples \
-#                                            --gpu 2 \
-#                                            --data ./data/records/snr_macho/$str/fold_0/ \
-#                                            --bs 2500 \
-#                                            --no-msk-token \
-#                                            --pe-base 10000 \
-#                                            --mask-format QK \
-#                                            --patience 20 \
-#                                            --m-alpha 1 \
-#                                            --lr 1e-5
-
-# done
-
+for str in ${nsamples[@]}; do
+   echo TRAINING WITH $str  
+   python -m presentation.scripts.pretrain --exp-name nsamples \
+                                           --gpu 0 \
+                                           --data ./data/records/macho/$str/fold_0/ \
+                                           --bs 2500 \
+                                           --no-msk-token \
+                                           --pe-base 10000 \
+                                           --mask-format K \
+                                           --patience 20 \
+                                           --m-alpha 1 \
+                                           --lr 1e-5
+done
+                                           
 # alphas=(-1000000000 -1000 -100 -10 -1 0 1)
 
 # for str in ${alphas[@]}; do
@@ -55,22 +54,21 @@
 
 # done
 
-probed=(0.6 0.8)
+# probed=(0.6 0.8)
+# for str in ${probed[@]}; do
+#    echo TRAINING WITH $str PROBED
+#    python -m presentation.scripts.pretrain --exp-name probed \
+#                                            --gpu 2 \
+#                                            --data ./data/shared/records/snr_macho/500000/fold_0 \
+#                                            --bs 2500 \
+#                                            --no-msk-token \
+#                                            --pe-base 10000 \
+#                                            --mask-format K \
+#                                            --probed $str
+#                                            --patience 20 \
+#                                            --m-alpha -100 \
+#                                            --temperature 2.5
+#                                            --lr 1e-5
 
-for str in ${probed[@]}; do
-   echo TRAINING WITH $str PROBED
-   python -m presentation.scripts.pretrain --exp-name probed \
-                                           --gpu 2 \
-                                           --data ./data/shared/records/snr_macho/500000/fold_0 \
-                                           --bs 2500 \
-                                           --no-msk-token \
-                                           --pe-base 10000 \
-                                           --mask-format K \
-                                           --probed $str
-                                           --patience 20 \
-                                           --m-alpha -100 \
-                                           --temperature 2.5
-                                           --lr 1e-5
-
-done
+# done
 
